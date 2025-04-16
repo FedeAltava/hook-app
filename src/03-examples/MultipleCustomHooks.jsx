e1a9311014f58614f806792c4aaa882170ad323d
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-16 16:37:12 +0200

    useFetch semi done

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,17 @@
+import { useFetch } from "../hooks"
 
 
 export const MultipleCustomHooks = () => {
 
+   const {data, isLoading, hasError, error} = useFetch('http://pokeapi.co/api/v2/pokemon/1');
 
   return (
-    <div>MultipleCustomHooks</div>
-  )
+    <>
+        <div>MultipleCustomHooks</div>
+        {isLoading&&<p>...Cargando</p>}
+        
+        <h2>{data?.name}</h2>
+    
+    </>
+)
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react"
+import { Message } from "../02-UseEffect/Message";
 
 
-export const useFetch = () => {
+export const useFetch = (url) => {
   const [state, setState ] = useState({
     data:null,
     isLoading:true,
@@ -11,11 +12,48 @@ export const useFetch = () => {
   });
 
   useEffect(()=>{
-    
-  })
+    getFetch()
+  },[url])
+
+   const setLoadingState = () =>{
+    setState({
+        data:null,
+        isLoading:true,
+        hasError:false,
+        error: null,
+    })
+   };
 
   const getFetch = async()=>{
-    fetch('http://pokeapi.co/api/v2/pokemon/1')
+    
+    setLoadingState();
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+    //sleep
+    await new Promise( resolve => setTimeout(resolve,1500));
+    if(!response.ok){
+        setState({
+            data:null,
+            isLoading:false,
+            hasError:true,
+            error:{
+                code:response.status,
+                Message:response.statusText,
+            }
+        })
+        return;
+    };
+
+
+    setState({
+        data:data,
+        isLoading:false,
+        hasError:false,
+        error:null,
+    })
+
   }
 
   const {data, isLoading, hasError, error} = state;
