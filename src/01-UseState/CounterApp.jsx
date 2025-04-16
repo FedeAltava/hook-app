5cac27f7cb663f7fa98f77b912921967cd21d209
fede altava <federico.altavaredondo@student.icareweb.com>
2025-04-16 13:05:34 +0200

    counterApp usestatae with object

--- src/01-UseState/CounterApp.jsx
+++ src/01-UseState/CounterApp.jsx
@@ -2,12 +2,26 @@ import { useState } from "react"
 
 
 export const CounterApp = () => {
-  const [ counter, setCounter ]= useState(10);
+  const [ state, setCounter ]= useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+  const {counter1, counter2, counter3} = state;
     return (
     <>
-        <h1>{counter}</h1>
+        <h1>{counter1}</h1>
+        <h1>{counter2}</h1>
+        <h1>{counter3}</h1>
+
         <hr />
-        <button onClick={() => setCounter(prev => prev + 1)}>+1</button>
+        <button onClick={() => setCounter({
+            ...state,
+            counter2:counter2+1
+        })
+        }>+1
+
+        </button>
 
     </>
   )
